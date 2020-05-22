880832ca5eedc868c87a0f529165ce028562fddd
Enoch <user@example.com>
2020-05-22 13:22:54 +0100

    Dependencies added

---
+++ app/src/main/java/com/example/themealdb/CategoriesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.themealdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CategoriesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categories)
+    }
+}
